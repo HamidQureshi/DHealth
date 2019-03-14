b94b081095bb076d110af40537ad581ea963b213
Hamid <hamid@activeledger.io>
2019-03-14 11:07:08 +0000

    removing project

--- app/src/main/java/com/example/hamid/dhealth/Utils/SwipeControllerActions.java
+++
@@ -1,9 +0,0 @@
-package com.example.hamid.dhealth.Utils;
-
-public abstract class SwipeControllerActions {
-
-    public void onLeftClicked(int position) {}
-
-    public void onRightClicked(int position) {}
-
-}
